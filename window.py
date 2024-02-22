cf59bf402780301cdeea160e9ed298dd4b580ae8
Star-Meow <sss37507617@gmail.com>
2024-02-22 15:36:57 +0800

    .

---
+++ window.py
@@ -0,0 +1,14 @@
+import random
+import tkinter as tk
+from PIL import Image, ImageTk
+from tkinter import messagebox
+
+
+root = tk.Tk()
+root.title("score-db")
+root.resizable(False, False) #視窗可縮放設定
+w,h = (800,600)
+root.geometry(str(w)+'x'+str(h)) #視窗大小
+
+
+root.mainloop()
