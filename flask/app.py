4b10f5b5a9a5e487aefdcba5486fbe715a52984d
Star-Meow <80087212+Star-Meow@users.noreply.github.com>
2024-05-16 23:06:07 +0800

    0516

--- flask/app.py
+++ flask/app.py
@@ -92,45 +92,102 @@ def team():
 
 @app.route('/party', methods=['POST'])
 def party():
-    try:
-        data = request.json
-        nowtime = time.strftime("%m-%d %H:%M")
-        print(data)
-        dbset(int(data['db']))
+    data = request.json
+    nowtime = time.strftime("%m-%d %H:%M")
+    print(data)
+    dbset(int(data['db']))
+    if data['type'] == 1:
         if data['memberA'] != data['memberB']:
-            if data['type'] == 1:
-                try:
+            memA = cursor.execute(
+                "SELECT * FROM score WHERE ID = ? ORDER BY ID DESC LIMIT 1", (data['memberA'],))
+            mA = memA.fetchone()
+            memB = cursor.execute(
+                "SELECT * FROM score WHERE ID = ? ORDER BY ID DESC LIMIT 1", (data['memberB'],))
+            mB = memB.fetchone()    
+            if mA == None or mB == None:
+                return jsonify({"success": False, "why": "資料庫找不到該學生!"})
+            else:
+                c = cursor.execute("SELECT * FROM team WHERE ID LIKE ?", ('%' + str(data['memberA']) + '%',))
+                stu1 = c.fetchone()
+                c = cursor.execute("SELECT * FROM team WHERE ID LIKE ?", ('%' + str(data['memberB']) + '%',))
+                stu2 = c.fetchone()
+
+                if stu1 == None and stu2 == None:
+                    if mA[2] > 40 and mB[2] > 40:
+
+                        c = cursor.execute("SELECT MAX(Party) FROM team")
+                        p_max = c.fetchone()
+                        p = 0
+                        if p_max[0] == None:
+                            p = 1
+                        else:
+                            p = int(p_max[0]) + 1
+
+                        cursor.execute(
+                            "INSERT INTO team (Party, mem, ID) VALUES (?, ?, ?)", 
+                            (p  ,2 , data['memberA'] + ',' + data['memberB']))
+                        cursor.execute(
+                            "UPDATE score SET score = ? WHERE ID = ?",
+                            (mA[2]-40 , data['memberA']))
+                        cursor.execute(
+                            "UPDATE score SET score = ? WHERE ID = ?",
+                            (mB[2]-40 , data['memberA']))
+                        cursor.execute(
+                            "INSERT INTO history (ID, action, info, time) VALUES (?, ?, ?, ?)",
+                            (data['memberA'], '兩人組隊扣除積分', '組隊積分扣除 40', time.strftime("%m-%d %H:%M")))
+                        cursor.execute(
+                            "INSERT INTO history (ID, action, info, time) VALUES (?, ?, ?, ?)",
+                            (data['memberB'], '兩人組隊扣除積分', '組隊積分扣除 40', time.strftime("%m-%d %H:%M")))
+                        
+                        connection.commit()
+                        return jsonify({'success': True, "why": "組隊成功!"})
+                    else:
+                        return jsonify({"success": False, "why": "學生分數不足!"})
+                else:
+                    return jsonify({"success": False, "why": "學生已在隊伍中!"})
+        else:
+            return jsonify({"success": False, "why": "學號重複!"})
+                
+    elif data['type'] == 2:
+        member = cursor.execute(
+            "SELECT * FROM score WHERE ID = ? ORDER BY ID DESC LIMIT 1", (data['member'],))
+        mem = member.fetchone()
+        group = cursor.execute(
+            "SELECT * FROM team WHERE Party = ? ORDER BY ID DESC LIMIT 1", (data['gp'],))
+        gp = group.fetchone()    
+        if mem == None: 
+            return jsonify({"success": False, "why": "資料庫找不到該學生!"})
+        if gp == None: 
+            return jsonify({"success": False, "why": "資料庫找不到該隊伍!"})
+        else:
+            c = cursor.execute("SELECT * FROM team WHERE ID LIKE ?", ('%' + str(data['member']) + '%',))
+            stu1 = c.fetchone()
+
+            if stu1 == None:
+                if gp != None:
+                    stu = gp[2].split(",")
+                    sc = 30 + 5 * (int(gp[1]) + 1) 
+                    
                     memA = cursor.execute(
-                        "SELECT * FROM score WHERE ID = ? ORDER BY ID DESC LIMIT 1", (data['memberA'],))
+                        "SELECT * FROM score WHERE ID = ? ORDER BY ID DESC LIMIT 1", (stu[0],))
                     mA = memA.fetchone()
                     memB = cursor.execute(
-                        "SELECT * FROM score WHERE ID = ? ORDER BY ID DESC LIMIT 1", (data['memberB'],))
-                    mB = memB.fetchone()                
-                except:
-                    return jsonify({"success": False, "why": "資料庫找不到該學生!"})
-                
-                if mA[2] < 40 or mB[2] < 40:
-                    c = cursor.execute("SELECT MAX(Party) FROM team")
-                    n_p = c.fetchone()
-                    cursor.execute(
-                        "INSERT INTO history (ID, action, info, time) VALUES (?, ?, ?, ?)",
-                        (mA[0], '兩人組隊扣除積分', '組隊積分扣除 40', time.strftime("%m-%d %H:%M")))
-                    cursor.execute(
-                        "INSERT INTO history (ID, action, info, time) VALUES (?, ?, ?, ?)",
-                        (mB[0], '兩人組隊扣除積分', '組隊積分扣除 40', time.strftime("%m-%d %H:%M")))
+                        "SELECT * FROM score WHERE ID = ? ORDER BY ID DESC LIMIT 1", (stu[1],))
+                    mB = memB.fetchone()
+                    if mA[2] > 40 and mB[2] > 40:
 
-                    return jsonify({"success": False, "why": "學生分數不足!"})
+                        return jsonify({'success': True, "why": "組隊成功!"})
+                    else:
+                        return jsonify({"success": False, "why": "學生分數不足!"})
                 else:
-                    return jsonify({'success': True})
-
-
-            elif data['type'] == 2:
-                return jsonify({'success': True})
-            elif data['type'] == 3:
-                return jsonify({'success': True})
+                    return jsonify({"success": False, "why": "找不到此隊伍!"})
+            else:
+                return jsonify({"success": False, "why": "學生已在隊伍中!"})
         
-    except:
-        return jsonify({'success': False})
+    elif data['type'] == 3:
+        return jsonify({'success': True})
+    else:
+        return jsonify({"success": False, "why": "學生已在隊伍中!"})
 
 
 if __name__ == '__main__':
